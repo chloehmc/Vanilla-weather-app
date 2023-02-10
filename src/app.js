0513ad921586137d279a75f9dde3bea399e1a221
chloehmc <122628488+chloehmc@users.noreply.github.com>
2023-02-10 18:33:41 +0100

    added current weather icon to be displayed from api

--- src/app.js
+++ src/app.js
@@ -37,12 +37,11 @@ function formatDate(timestamp) {
 }
 
 function displayTemperature(response) {
-  //console.log(response.data);
   let temperatureElement = document.querySelector("#currentTemperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
-  let discriptionElement = document.querySelector("#description");
+  let discriptionElement = document.querySelector("#weatherDescription");
   discriptionElement.innerHTML = response.data.condition.description;
   let feelsLikeElement = document.querySelector("#feelsLike");
   feelsLikeElement.innerHTML = Math.round(response.data.temperature.feels_like);
@@ -52,9 +51,12 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#dateTime");
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute("src", response.data.condition.icon_url);
+  iconElement.setAttribute("alt", response.data.condition.description);
 }
 
 let apiKey = "06740ta7fb3c0b6bo8bced4f1a40e926";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Lisbon&key=06740ta7fb3c0b6bo8bced4f1a40e926&units=metric`;
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Amsterdam&key=06740ta7fb3c0b6bo8bced4f1a40e926&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
